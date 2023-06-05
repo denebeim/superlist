f6bc50ee896b6489bd0dac7474008da967b91be7
Denebeim, Joy <JoyDenebeim@quickenloans.com>
2023-06-05 15:37:11 -0700

    Functional test now checks that we can input to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,9 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,15 +19,31 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        assert 'To-Do' in self.browser.title, "Browser title was " + self.browser.title
+        self.assertIn('To-Do', self.browser.title, self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1')
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby is tying
         # fly-fishing lures)
+        inputbox.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table=self.browser.find_element(By.ID,'id_list_table')
+        rows=table.find_elements(By.TAG_NAME,'tr')
+        self.assertTrue(
+            any(row.text=="Buy peacock feathers"for row in rows)
+        )
 
         # There is still a text box inviting her to add another item. She enters
         # "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -38,6 +57,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
         self.fail('Finish the test')
+
 
 if __name__ == 'main':
     unittest.main()
