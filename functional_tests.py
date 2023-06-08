023ffb17a22e69f8e466ad23e01bed7c2b55bed2
Denebeim, Joy <JoyDenebeim@quickenloans.com>
2023-06-08 10:02:38 -0700

    Refactor

--- functional_tests.py
+++ functional_tests.py
@@ -32,31 +32,27 @@ class NewVisitorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby is tying
         # fly-fishing lures)
-        inputbox.send_keys("Buy peacock feathers")
+        row_text = "Buy peacock feathers"
+        inputbox.send_keys(row_text)
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table(f"1: {row_text}")
 
         # There is still a text box inviting her to add another item. She enters
         # "Use peacock feathers to make a fly" (Edith is very methodical)
+        row_text2 = "Use peacock feathers to make a fly"
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(row_text2)
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                      [row.text for row in rows]
-                      )
+        self.check_for_row_in_list_table(f"1: {row_text}")
+        self.check_for_row_in_list_table(f"2: {row_text2}")
 
         # Edith wonders whether the site will remember her list.  Then she sees that the site
         # has generated a unique URL for her -- there is som explanatory text to that effect.
@@ -65,6 +61,12 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
         self.fail('Finish the test')
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        return rows
 
 
 if __name__ == 'main':
