d9bc23877199d5238b90cee860f309ef722bf411
Denebeim, Joy <JoyDenebeim@quickenloans.com>
2023-06-09 13:40:28 -0700

    Create the model for multiple lists

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+class List(models.Model):
+    pass
 
-# Create your models here.
+
 class Item(models.Model):
-    text=models.TextField(default='')
-
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=None, default=None)
