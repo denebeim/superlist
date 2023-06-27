664433ad19d1aeaeaa4f068f9cfc7f1278fed0a0
Denebeim, Joy <JoyDenebeim@quickenloans.com>
2023-06-27 14:38:33 -0700

    make prod settings variable

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,13 +19,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k*h3=fch5q1(zni6j-s0sxg@bulkz1+upkfx$xxbngj!61k8vc'
+if 'DHANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'k*h3=fch5q1(zni6j-s0sxg@bulkz1+upkfx$xxbngj!61k8vc'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
