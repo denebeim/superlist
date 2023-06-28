faaf96f18252924f4b4e2c6b39a19ea492e4fa9f
Denebeim, Joy <JoyDenebeim@quickenloans.com>
2023-06-28 14:20:45 -0700

    Refactor list view to add item on POST

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -55,6 +55,13 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_=List.objects.create()
+        response = self.client.post(f'/lists/{list_.id}/', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 
 
 class NewListTest(TestCase):
@@ -71,13 +78,15 @@ class NewListTest(TestCase):
         self.assertRedirects(response, f'/lists/{new_list.id}/',
                              fetch_redirect_response=False
                              )
+
     def test_validation_errors_are_sent_back_to_home_page_template(self):
         response = self.client.post('/lists/new', data={'item_text': ''})
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error=escape("You can't have an empty list item")
-        self.assertContains(response,expected_error)
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
     def test_invalid_list_items_arent_saved(self):
         response = self.client.post('/lists/new', data={'item_text': ''})
-        self.assertEqual(List.objects.count(),0)
-        self.assertEqual(Item.objects.count(),0)
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
