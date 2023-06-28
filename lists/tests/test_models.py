60694208d960a390b7efe0fb78c18d07e8340cec
Denebeim, Joy <JoyDenebeim@quickenloans.com>
2023-06-28 11:25:34 -0700

    refactor separate model and view tests

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -35,30 +35,6 @@ def _create_two_lists() -> object:
     return correct_list, other_list
 
 
-class ListViewTest(TestCase):
-    def test_uses_list_template(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        response = self.client.get(f'/lists/{list_.id}/')
-        self.assertTemplateUsed(response, 'list.html')
-
-    def test_displays_only_list_items_for_that_list(self):
-        correct_list, other_list = _create_two_lists()
-
-        response = self.client.get(f'/lists/{correct_list.id}/')
-
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
-        self.assertNotContains(response, 'Other list item 1')
-        self.assertNotContains(response, 'Other list item 2')
-
-    def test_passes_correct_list_to_template(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-        response = self.client.get(f'/lists/{correct_list.id}/')
-        self.assertEqual(response.context['list'], correct_list)
-
-
 class NewItemTest(TestCase):
     S = 'A new item to an existing list'
 
