3fd559458aa6a2cee3fe8e32b4395ebf945c589a
Denebeim, Joy <JoyDenebeim@quickenloans.com>
2023-06-28 11:03:49 -0700

    Functional tests expansion

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,39 +1,11 @@
-import os
-import time
-import unittest
-
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 10
+from functional_tests.base import FunctionalTest
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element(By.ID, 'id_list_table')
-                rows = table.find_elements(By.TAG_NAME, 'tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(.5)
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_for_one_user_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes to check out its homepage.
@@ -60,7 +32,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
 
-        self.wait_for_row_in_list_table(f"1: {row_text}")
+        self.wait_for(lambda: self.wait_for_row_in_list_table(f"1: {row_text}"))
 
         # There is still a text box inviting her to add another item. She enters
         # "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -70,9 +42,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        self.wait_for_row_in_list_table(f"1: {row_text}")
-        self.wait_for_row_in_list_table(f"2: {row_text2}")
-        # Satisfied, she goes back to sleep
+        self.wait_for(lambda: self.wait_for_row_in_list_table(f"1: {row_text}"))
+        self.wait_for(lambda: self.wait_for_row_in_list_table(f"2: {row_text2}"))
 
         # Edith wonders whether the site will remember her list.
 
@@ -83,7 +54,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for(lambda: self.wait_for_row_in_list_table('1: Buy peacock feathers'))
 
         # Then she sees that
         # the site has generated a unique URL for her -- there is some explanatory
@@ -109,7 +80,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Buy Milk')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy Milk')
+        self.wait_for(lambda: self.wait_for_row_in_list_table('1: Buy Milk'))
 
         # Francis gets his own Unique URL
         francis_list_url = self.browser.current_url
@@ -120,34 +91,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
-
-    def test_layout_and_styling(self):
-        # Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # She notices the input box is nicely formatted
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-        # She starts a new test and sees the input is nicely centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-
-# Satisfied, they both go back to sleep
-
-
-if __name__ == 'main':
-    unittest.main()
