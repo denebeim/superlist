354b5614e2ae9021f8e69f33d4f3c8af382bdf3a
Denebeim, Joy <JoyDenebeim@quickenloans.com>
2023-07-01 15:20:18 -0700

    Logging in

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 import accounts
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 from accounts.models import Token
 
@@ -43,12 +43,13 @@ class SendLoginEmailViewTest(TestCase):
         self.assertEqual(message.tags, "success")
 
 
+@patch('accounts.views.auth')
 class LoginViewTest(TestCase):
-    def test_redirects_to_home_page(self):
+    def test_redirects_to_home_page(self, mock_auth):
         response = self.client.get('/accounts/login?token=abcd123')
         self.assertRedirects(response, '/')
 
-    def test_creates_token_associated_with_email(self):
+    def test_creates_token_associated_with_email(self, mock_auth):
         self.client.post(
             '/accounts/send_login_email', data={'email': WILLING_TEST_SUBJECT}
         )
@@ -56,7 +57,7 @@ class LoginViewTest(TestCase):
         self.assertEqual(token.email, WILLING_TEST_SUBJECT)
 
     @patch('accounts.views.send_mail')
-    def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail,mock_auth):
         self.client.post(
             '/accounts/send_login_email', data={'email': WILLING_TEST_SUBJECT}
         )
@@ -64,3 +65,21 @@ class LoginViewTest(TestCase):
         expected_url = f'http://testserver/accounts/login?token={token.uid}'
         (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
         self.assertIn(expected_url, body)
+
+    def test_calls_authenticate_with_uid_from_get_request(self, mock_auth):
+        self.client.get('/accounts/login?token=abc123')
+        self.assertEqual(
+            mock_auth.authenticate.call_args,
+            call(uid='abc123')
+        )
+
+    def test_calls_auth_login_with_user_if_there_is_one(self, mock_auth):
+        response = self.client.get('/accounts/login?token=abc123')
+        self.assertEqual(
+            mock_auth.login.call_args,
+            call(response.wsgi_request, mock_auth.authenticate.return_value)
+        )
+    def test_does_not_login_if_user_is_not_authenticated(self,mock_auth):
+        mock_auth.authenticate.return_value=None
+        self.client.get('/accounts/login?token=abc123')
+        self.assertEqual(mock_auth.login.called,False)
--- superlists/settings.py
+++ superlists/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS=['accounts.authentication.PasswordlessAuthenticationBackend',]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
